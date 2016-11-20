c0cb5513b2e192608b88792e8e9380c73a5ab74a
adamtoth9112 <adamtoth9112@gmail.com>
2016-11-20 20:28:54 +0100

    Database export

--- app/components/side-nav/sidenav.directive.js
+++ app/components/side-nav/sidenav.directive.js
@@ -43,6 +43,12 @@
       vm.visible = function(ev) {
         return ProjectService.getActualProject() != null;
       };
+
+      vm.export = function (ev) {
+        var url = "https://arepapp-358ea.firebaseio.com/.json?print=pretty&format=export&download=arepapp-358ea-export.json&auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0Nzk2NjkxMzEsImV4cCI6MTQ3OTY3MjczMSwiYWRtaW4iOnRydWUsInYiOjB9.wBFtn0tcytiDQfCdlONO2oKNvl8okpY70iNHDlHPduw";
+        window.open(url, '_blank');
+        window.focus();
+      };
     }
   }
 
